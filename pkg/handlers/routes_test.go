22441fcce0c7107e2b76797f9f51f4b797376ef1
Igor Komlew <igor.komlew@gmail.com>
2019-03-31 23:03:21 +0200

    added simple UI to test SSE rendering

--- pkg/handlers/routes_test.go
+++ pkg/handlers/routes_test.go
@@ -1,35 +1,53 @@
 package handlers
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestRouter(t *testing.T) {
-	// r := Router()
-	// ts := httptest.NewServer(r)
-	// defer ts.Close()
+func TestPingRouter(t *testing.T) {
+	re := Resources{}
+	r := re.Router()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
 
-	// res, err := http.Get(ts.URL + "/ping")
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// if res.StatusCode != http.StatusOK {
-	// 	t.Errorf("Status code for /ping is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusOK)
-	// }
+	res, err := http.Get(ts.URL + "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code for /ping is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusOK)
+	}
 
-	// res, err = http.Post(ts.URL+"/ping", "text/plain", nil)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// if res.StatusCode != http.StatusMethodNotAllowed {
-	// 	t.Errorf("Status code for /ping is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusMethodNotAllowed)
-	// }
+	res, err = http.Post(ts.URL+"/ping", "text/plain", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status code for /ping is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusMethodNotAllowed)
+	}
 
-	// res, err = http.Get(ts.URL + "/not-exists")
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// if res.StatusCode != http.StatusNotFound {
-	// 	t.Errorf("Status code for /home is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusNotFound)
-	// }
+	res, err = http.Get(ts.URL + "/not-exists")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusNotFound {
+		t.Errorf("Status code for /home is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusNotFound)
+	}
+}
+
+func TestHealth(t *testing.T) {
+	re := Resources{}
+	r := re.Router()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL + "/health")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Status code for /health is wrong. Have: %d, want: %d.", res.StatusCode, http.StatusOK)
+	}
 }
