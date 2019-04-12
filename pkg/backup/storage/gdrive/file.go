4d0934ad765168f66b9312f7d6a4b85c5d4058e4
Igor Komlew <igor.komlew@gmail.com>
2019-04-12 12:28:18 +0200

    removed debug

--- pkg/backup/storage/gdrive/file.go
+++ pkg/backup/storage/gdrive/file.go
@@ -11,24 +11,18 @@ import (
 // FindFile ...
 // For testing: https://developers.google.com/drive/api/v3/reference/files/list
 func (s *Storage) FindFile(name, folderID string) (*drive.File, error) {
-	fmt.Printf(">>> FindFile(): %s\n", name)
-
 	q := fmt.Sprintf("mimeType != 'application/vnd.google-apps.folder' and trashed = false and name = '%s' and '%s' in parents", name, folderID)
 	files, err := s.service.Files.List().Q(q).Do()
 
 	if err != nil {
-		fmt.Printf(">>> gdrive.FindFile(): file [%s] not found [%v]\n", name, err)
-		fmt.Printf("q=%s\n", q)
 		return nil, err
 	}
 
 	if len(files.Files) == 0 {
-		fmt.Printf(">>> gdrive.FindFile(): file [%s] not found in [%s]\n", name, folderID)
 		return nil, nil
 	}
 
 	if len(files.Files) == 1 {
-		fmt.Printf(">>> gdrive.FindFile(): found 1 file [%s]\n", files.Files[0].Name)
 		return files.Files[0], nil
 	}
 	for _, file := range files.Files {
@@ -39,7 +33,6 @@ func (s *Storage) FindFile(name, folderID string) (*drive.File, error) {
 
 // CreateOrUpdateFile ...
 func (s *Storage) CreateOrUpdateFile(fromFile *os.File, fileName, mimeType, folderID string) (*drive.File, error) {
-	fmt.Printf(">>> CreateOrUpdateFile(): %s\n", fileName)
 	mu.Lock()
 	defer mu.Unlock()
 
@@ -59,7 +52,6 @@ func (s *Storage) CreateOrUpdateFile(fromFile *os.File, fileName, mimeType, fold
 	}
 
 	if err == nil && file != nil {
-		log.Printf("gdrive.CreateOrUpdateFile(): try to update file [%s] with id [%s]\n", file.Name, file.Id)
 		file, err = s.service.Files.Update(file.Id, nil).Media(fromFile).Do()
 		return file, err
 	}
